5a2c4b9d24e6385c62df8b2a0b0d1a4ceb4079ad
preeti9311 <174781309+preeti9311@users.noreply.github.com>
2026-08-17 15:09:27 +0530

    data validation

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from textSummarizer.logging.logger import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_ingestion_pipeline = DataIngestionTrainingPipeline()
     data_ingestion_pipeline.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed!<<<<<\n\nx==========x")
+except Exception as e:
+    logger.error(f"Error occurred while running stage {STAGE_NAME}: {e}")
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_validation_pipeline = DataValidationTrainingPipeline()
+    data_validation_pipeline.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed!<<<<<\n\nx==========x")
 except Exception as e:
     logger.error(f"Error occurred while running stage {STAGE_NAME}: {e}")
